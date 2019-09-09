8ab39791cb7de6ca93c97b0a9d8cec95ced82a7d
Jeremy Hull <jchull@gmail.com>
2019-09-09 15:56:40 -0600

    remove password and mongo _id fields

--- src/types/TeslaAccount.ts
+++ src/types/TeslaAccount.ts
@@ -1,11 +1,8 @@
 export interface ITeslaAccount {
-  _id: string;
   token_created_at?: number;
   token_expires_in?: number;
   refresh_token?: string;
   access_token?: string;
   email: string;
-  // Does not get persisted
-  password: string;
 }
 
