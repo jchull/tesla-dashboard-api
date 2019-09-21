4e34da6fa6ff5a8f8d7807ced9154c964c0ceb4d
Jeremy Hull <jchull@gmail.com>
2019-09-21 06:42:14 -0600

    add fields for polling settings

--- src/types/SyncPreferences.ts
+++ src/types/SyncPreferences.ts
@@ -1,27 +1,34 @@
 export interface ISyncPreferences {
   _id: string;
   enabled: boolean;
+  account_id?: string;
+  sleepTriggerSeconds: number;
   charging_minDurationMinutes: number;
   charging_costPerKwhHome: number;
   charging_costPerKwhSupercharging: number;
+  charging_maxGapSeconds: number;
   /**
    * tuple of polling intervals in seconds to use while charging
    * level 1 charging = [0], level 2 = [1], etc
    */
   charging_pollingIntervalsSeconds: [number, number, number];
 
-  driving_pollingIntervalsSeconds: number;
+  driving_pollingIntervalSeconds: number;
   driving_minDurationMinutes: number;
+  driving_maxGapSeconds: number;
 }
 
 
 export const DEFAULT_SYNC_PREFERENCES: ISyncPreferences = {
   _id: 'default',
   enabled: false,
-  driving_pollingIntervalsSeconds: 60,
-  driving_minDurationMinutes: 5,
+  charging_maxGapSeconds: 300,
   charging_minDurationMinutes: 5,
   charging_costPerKwhHome: 0.12,
   charging_costPerKwhSupercharging: 0.28,
-  charging_pollingIntervalsSeconds: [600, 200, 30]
+  charging_pollingIntervalsSeconds: [600, 200, 30],
+  driving_maxGapSeconds: 300,
+  driving_pollingIntervalSeconds: 60,
+  driving_minDurationMinutes: 5,
+  sleepTriggerSeconds: 300
 };
